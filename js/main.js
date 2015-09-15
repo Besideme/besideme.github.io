6fc0d2ef5742bd7670804f6e98deb57e1464ff56
Besideme <leichaomin@126.com>
2015-09-15 11:52:32 +0800

    baidu-share

---
+++ js/main.js
@@ -0,0 +1,24 @@
+(function(){
+    backToTop("#back-top");
+    function backToTop(toTopId) {
+        // hide #back-top first
+        $(toTopId).hide();
+
+        // fade in #back-top
+        $(window).scroll(function() {
+            if ($(this).scrollTop() > 300) {
+                $(toTopId).fadeIn();
+            } else {
+                $(toTopId).fadeOut();
+            }
+        });
+
+        // scroll body to 0px on click
+        $(toTopId + ' a').click(function() {
+            $('body,html').animate({
+                scrollTop: 0
+            }, 100);
+            return false;
+        });
+    } 
+});
